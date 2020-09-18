a7f0571e426abcfba6644f72b0b9398c4fc0bae8
sizhky <1992chinna@gmail.com>
2020-09-18 18:28:50 +0530

    Update model.py

--- model.py
+++ model.py
@@ -15,8 +15,10 @@ class CSRNet(nn.Module):
         if not load_weights:
             mod = models.vgg16(pretrained = True)
             self._initialize_weights()
-            for i in xrange(len(self.frontend.state_dict().items())):
-                self.frontend.state_dict().items()[i][1].data[:] = mod.state_dict().items()[i][1].data[:]
+            items = list(self.frontend.state_dict().items())
+            _items = list(mod.state_dict().items())
+            for i in range(len(self.frontend.state_dict().items())):
+                items[i][1].data[:] = _items[i][1].data[:]
     def forward(self,x):
         x = self.frontend(x)
         x = self.backend(x)
